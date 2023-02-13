8162fae8df7655aa30ce9cbabb3dc85f375a320b
Tarsha <tarsha.t.simmons@gmail.com>
2023-02-12 19:10:50 -0600

    Initial commit

---
+++ python_app.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+  str = "Hi everyone, I am Tasha and hello hi hi"
+  word_to_replace = input("Enter the word to replace: ")
+  word_replacement = input("Enter the word replacement: ")
+  print(str.replace(word_to_replace, word_replacement))
+
+  replace_word()
