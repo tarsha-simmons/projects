6e1f9a32f2b87b1568dd6cf8c841b4b732203667
Tarsha <102608343+tarsha-simmons@users.noreply.github.com>
2023-10-13 14:11:12 -0500

    refreshing memory coding skills
    refreshing

---
+++ 423.PY/app.py
@@ -0,0 +1,3 @@
+# refresher of python, 2023. Building proects
+character_name = 'John'
+character_age = '35'
---
+++ FCC PY/madlibs.py
@@ -0,0 +1,10 @@
+# string concatenation is how we put strings together
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = ("Famous person ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+  I love to {verb1}. Stay hydrated abd {verb2} like you are {famous_person}!"
+
+print(madlib)
